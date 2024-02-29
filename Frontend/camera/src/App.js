bc2adc3faafec6b8349f4045b115998888a99e33
Neeraj Verma <neeraj708055@gmail.com>
2024-02-29 14:40:07 +0530

    new new

--- Frontend/camera/src/App.js
+++ Frontend/camera/src/App.js
@@ -4,8 +4,9 @@ import './App.css';
 
 const App = () => {
   const [video, setVideo] = useState('');
+  const [video1, setVideo1] = useState('');
   const [capturedImages, setCapturedImages] = useState([]);
-  const [showCapturedImages, setShowCapturedImages] = useState(false);
+  const [capturedImages1, setCapturedImages1] = useState([]);
 
   useEffect(() => {
     const ws = new WebSocket('ws://localhost:5000/video_feed');
@@ -17,6 +18,7 @@ const App = () => {
     ws.onmessage = (event) => {
       const data = JSON.parse(event.data);
       setVideo(data.image_1);
+      setVideo1(data.image_2);
     };
 
     ws.onclose = () => {
@@ -29,46 +31,49 @@ const App = () => {
   }, []);
 
   const handleCapture = () => {
-    axios.post('http://localhost:5000/capture_frame',{
-      method: 'POST'
-    })
-    .then(data =>{
-      // console.log(data)
-      // console.log(data['data']);
-      // let new_img = 'data:image/jpeg;base64'+data['data'];
-      setCapturedImages([...capturedImages,data['data']])
-    })
-    .catch(error => console.error('Error capturing frame:', error));
-  };
-
-  const toggleShowCapturedImages = () => {
-    setShowCapturedImages(!showCapturedImages);
+    axios.post('http://localhost:5000/capture_frame', {
+        method: 'POST'
+      })
+      .then(data => {
+        setCapturedImages([...capturedImages, data.data['data']]);
+        setCapturedImages1([...capturedImages1, data.data['data1']]);
+      })
+      .catch(error => console.error('Error capturing frame:', error));
   };
 
   return (
     <div className="dashboard">
+      <button onClick={handleCapture} className='btn'>Capture</button>
       <div className="video-stream">
         <h1>Video Stream</h1>
         <div className="video-container">
-          {video && <img className="video" src={`data:image/jpeg;base64,${video}`} alt="video stream" />}
+          {video && <img className="video1" src={`data:image/jpeg;base64,${video}`} alt="video stream" />}
+          {video1 && <img className="video2" src={`data:image/jpeg;base64,${video1}`} alt="video stream 1" />}
         </div>
       </div>
+
       <div className="captured-images">
-        <h1>Captured Images</h1>
-        <button onClick={handleCapture}>Capture</button>
-        <button onClick={toggleShowCapturedImages}>
-          {showCapturedImages ? 'Hide Captured Images' : 'Show Captured Images'}
-        </button>
-        {showCapturedImages && (
-          <div className="captured-images-grid">
-            {capturedImages.map((image, index) => (
-              <div key={index} className="captured-image">
-                <h2>Captured Image {index + 1}</h2>
-                <img src={`data:image/jpeg;base64,${image}`} alt={`captured image ${index + 1}`} />
-              </div>
-            ))}
-          </div>
-        )}
+        <h1>Camera 1</h1>
+        <div className="captured-images-grid">
+          {capturedImages.map((image, index) => (
+            <div key={index} className="captured-image">
+              <h2>Captured Image {index + 1}</h2>
+              <img src={`data:image/jpeg;base64,${image}`} alt={`captured image ${index + 1}`} />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="captured-images1">
+        <h1>Camera 2</h1>
+        <div className="captured-images-grid">
+          {capturedImages1.map((image, index) => (
+            <div key={index} className="captured-image">
+              <h2>Captured Image {index + 1}</h2>
+              <img src={`data:image/jpeg;base64,${image}`} alt={`captured image ${index + 1}`} />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- app.py
+++ app.py
@@ -1,80 +1,146 @@
-from gevent import pywsgi, monkey
-monkey.patch_all()
+from gevent import pywsgi
 from flask import Flask
 from flask_sockets import Sockets
 from geventwebsocket.handler import WebSocketHandler
 import cv2
 import base64
-import threading 
-import numpy as np
+import threading
+import gevent
 import json
-import datetime
 import time
 import os
+from gevent import pywsgi
+import datetime
 from flask_cors import CORS
+
 app = Flask(__name__)
 sockets = Sockets(app)
 CORS(app)
 
-
-
-rtsp_url = r'C:\Users\DSI-LPT-006\Desktop\Slab\video.mp4'
+rtsp_url = 'rtsp://admin:Deevia@123@192.168.1.7:554/?h264x=4'
 
 class FrameGenerator:
     def __init__(self, rtsp_url):
         self.curr_frame = None
+        self.curr_frame1=None
         self.cap = cv2.VideoCapture(rtsp_url)
+        self.cap1=cv2.VideoCapture(0)
+        self.camera_connected=True
         if not self.cap.isOpened():
             print("Error: Couldn't open the camera.")
+            self.camera_connected = False
+        else:
+            self.camera_connected = True
 
     def generate_frames(self):
         try:
-            while True: 
-                ret, frame = self.cap.read()
-                if not ret:
-                    frame = np.zeros((480, 640, 3), dtype=np.uint8)
+            while True:
+                if self.camera_connected:
+                    
+                    ret, frame = self.cap.read()
+                    if not ret:
+                        print("Error: Couldn't read frame from the camera.")
+                        self.camera_connected = False
+                        continue
+                    self.curr_frame = frame
                 
-                self.curr_frame = frame
-                time.sleep(0.01)
+                else:
+                    self.cap.release()
+                    self.cap = cv2.VideoCapture(rtsp_url)
+                    if self.cap.isOpened():
+                        print("Camera reconnected.")
+                        self.camera_connected = True
+                    else:
+                        time.sleep(1) 
+                time.sleep(0.01) 
+
         except Exception as e:
             print("Error in generating frame:", e)
 
+    def generate_frames1(self):
+        try:
+            while True:
+                if self.camera_connected:
+                    ret,frame=self.cap1.read()
+
+                    if not ret:
+                        print("Error : Couldnt read frame from the camera")
+                        self.camera_connected=False
+                        continue
+                    self.curr_frame1=frame
+                else:
+                    self.cap1.release()
+                    self.cap1=cv2.VideoCapture(0)
+                    if self.cap1.isOpened():
+                        print("Camera Reconnected")
+                        self.camera_connected=True
+                    else:
+                        time.sleep(1)
+                    time.sleep(0.01)
+        except Exception as e:
+             print("Error in generating frame:", e)
 frame_generator = FrameGenerator(rtsp_url)
 
 @sockets.route('/video_feed')
 def video_feed_socket(ws):
     while True:
         frame = frame_generator.curr_frame
-        if frame is not None:
+        frame1=frame_generator.curr_frame1
+        if frame is not None and frame1 is not None :
             outFrame = cv2.imencode('.jpg', frame)[1].tobytes()
+            outframe1 = cv2.imencode('.jpg', frame1)[1].tobytes()
             image_64_encode = base64.b64encode(outFrame).decode('ascii')
-            result = json.dumps({'image_1': image_64_encode})
+            image_64_encode1 = base64.b64encode(outframe1).decode('ascii')
+            result = json.dumps({'image_1': image_64_encode,'image_2': image_64_encode1})
             if not ws.closed:
                 ws.send(result)
-            time.sleep(0.01)
+        gevent.sleep(0.01)
 
 @app.route('/capture_frame', methods=['POST'])
 def capture_frame():
-    try: 
-        _, frame = frame_generator.cap.read()
-        _, buffers = cv2.imencode('.jpg', frame)
-        image_64_encode = base64.b64encode(buffers).decode('ascii')
-        
-        image_data = base64.b64decode(image_64_encode)
-        save_path = r'C:\Users\DSI-LPT-006\Desktop\Capture Images'
-        os.makedirs(save_path, exist_ok=True)
-        current_time = datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
-        file_path = os.path.join(save_path, f"captured_frame_{current_time}.jpg")
-        with open(file_path, "wb") as f:
-            f.write(image_data)
-        
-        return image_64_encode, 200
+    print('capture_frame')
+    try:
+        frame = frame_generator.curr_frame
+        frame1=frame_generator.curr_frame1
+        if frame is not None and frame1 is not None:
+          
+            save_dir=r'C:\Users\DSI-LPT-006\Desktop\Data'
+            today_date = datetime.datetime.now().strftime("%d-%m-%Y")
+            cam1_dir = os.path.join(save_dir,'cam1',today_date)
+            cam2_dir = os.path.join(save_dir,'cam2',today_date)
+         
+            os.makedirs(cam1_dir, exist_ok=True)
+            os.makedirs(cam2_dir, exist_ok=True)
+            current_time = datetime.datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
+            file_name = f"cam_1_{current_time}.jpg"
+            file_name1 = f"cam_2_{current_time}.jpg"
+            file_path = os.path.join(cam1_dir, file_name)
+            file_path1 = os.path.join(cam2_dir, file_name1)
+            cv2.imwrite(file_path, frame)
+            cv2.imwrite(file_path1, frame1)
+            print(f"Frame captured and saved at: {file_path}")
+            print(f"Frame2 captured and saved at: {file_path1}")
+            
+            _, buffer = cv2.imencode('.jpg', frame)
+            _, buffer1 = cv2.imencode('.jpg', frame1)
+            encoded_string = base64.b64encode(buffer).decode('utf-8')
+            encoded_string1 = base64.b64encode(buffer1).decode('utf-8')
+            
+           
+            return json.dumps({'data': encoded_string,'data1':encoded_string1, 'file_path': file_path,'file_path1':file_path1}), 200
+        else:
+            return "Error: No frame available to capture", 500
+
     except Exception as e:
         return str(e), 500
+
 
 if __name__ == '__main__':
     t1 = threading.Thread(target=frame_generator.generate_frames)
     t1.start()
+    t2 = threading.Thread(target=frame_generator.generate_frames1)
+    t2.start()
+  
     server = pywsgi.WSGIServer(('0.0.0.0', 5000), app, handler_class=WebSocketHandler)
     print("Server running...")
     server.serve_forever()
